8ab9292d71208db0a244ca08eb18cb53d23f5af9
马博文 <2636965214@qq.com>
2020-05-20 10:11:30 +0800

    .

---
+++ commons-api/src/main/java/com/github/mbw/commons/api/request/BaseRequest.java
@@ -0,0 +1,10 @@
+package com.github.mbw.commons.api.request;
+
+/**
+ * TODO
+ *
+ * @author Mabowen
+ * @date 2020-05-20 09:35
+ */
+public interface BaseRequest {
+}
---
+++ commons-api/src/main/java/com/github/mbw/commons/api/response/BaseResponse.java
@@ -0,0 +1,10 @@
+package com.github.mbw.commons.api.response;
+
+/**
+ * TODO
+ *
+ * @author Mabowen
+ * @date 2020-05-20 09:36
+ */
+public interface BaseResponse {
+}
