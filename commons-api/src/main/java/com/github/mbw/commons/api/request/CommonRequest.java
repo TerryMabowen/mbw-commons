50678737f1129c206849455e6363c81e4e180fd3
马博文 <2636965214@qq.com>
2020-05-20 10:14:53 +0800

    .

---
+++ commons-api/src/main/java/com/github/mbw/commons/api/request/CommonRequest.java
@@ -0,0 +1,10 @@
+package com.github.mbw.commons.api.request;
+
+/**
+ * TODO
+ *
+ * @author Mabowen
+ * @date 2020-05-20 10:14
+ */
+public class CommonRequest implements BaseRequest{
+}
---
+++ commons-api/src/main/java/com/github/mbw/commons/api/response/CommonResponse.java
@@ -0,0 +1,10 @@
+package com.github.mbw.commons.api.response;
+
+/**
+ * TODO
+ *
+ * @author Mabowen
+ * @date 2020-05-20 10:14
+ */
+public class CommonResponse implements BaseResponse{
+}
